ec053241148cbbabae50debe32c877cf23d9dc23
Kareem Taiye <olayinkahassan117@gmail.com>
2026-06-30 22:28:11 +0100

    Added 2-1 MUX module for 8-bit busses

--- ch4-combinational-logic/structural_mux4.sv
+++ ch4-combinational-logic/structural_mux4.sv
@@ -13,3 +13,11 @@ module mux4(input logic[3:0]d0, d1, d2, d3,
     mux2 muxhigh(d2, d3, s[0], high);
     mux2 muxfinal(low, high, s[1], y);
 endmodule 
+
+
+module mux2_8(input logic[7:0]d0, d1, 
+              input logic s, 
+              output logic[7:0]y);
+    mux2(d0[3:0], d1[3:0], s, y[3:0]);
+    mux2(d0[7:4], d1[7:4], s, y[7:4]);
+endmodule

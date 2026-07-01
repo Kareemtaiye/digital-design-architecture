6c9ed6382efa081ba2e2566a06e242b9e28211b8
Kareem Taiye <olayinkahassan117@gmail.com>
2026-07-01 16:42:08 +0100

    Added resettable register module, synch and asynch

--- ch5-sequential-logic/resettable_reg.sv
+++ ch5-sequential-logic/resettable_reg.sv
@@ -7,14 +7,13 @@ module flopr(input logic clk,
     always_ff @(posedge clk, posedge reset)
         if(reset) q <= 4'b0;
         else q <= d;
-        
 endmodule
 
 module flopr(input logic clk,
              input logic reset.
              input logic[3:0]d,
              output logic[3:0]q);
-             
+
     //synchronous reset
     always_ff @(posedge clk)
         if(reset) q <= 4'b0;

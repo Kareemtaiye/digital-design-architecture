34e0722c546a2361d8eeaa41c3e10c645186f51a
Kareem Taiye <olayinkahassan117@gmail.com>
2026-07-01 21:21:19 +0100

    renamed ch4 module

---
+++ ch4-sequential-logic/flop.sv
@@ -0,0 +1,6 @@
+module flop(input logic clk, 
+            input logic[3:1]d,
+            output logic[3:0]q);
+    always_ff @(posedge clk)
+        q <= d; // q gets d ONLY on the rising edge of the clk
+endmodule

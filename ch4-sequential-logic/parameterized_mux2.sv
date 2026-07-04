b39fefaa05fd6c79163b934b242d4a951c6d5a68
Kareem Taiye <olayinkahassan117@gmail.com>
2026-07-04 11:51:05 +0100

    Added 8-bit and 12-bit 4-1 MUX using parametized 2-1 MUXes

--- ch4-sequential-logic/parameterized_mux2.sv
+++ ch4-sequential-logic/parameterized_mux2.sv
@@ -6,3 +6,23 @@ module mux2
 
     assign y = s ? d1 : d0; 
 endmodule
+
+module mux4_8(input logic[7:0]d0, d1, d2, d3,
+              input logic[1:0]s,
+              output logic[7:0]y);
+    logic[7:0]low, high;
+
+    mux2 lowmux(d0, d1, s[0], low);
+    mux2 highmux(d2, d3, s[0], high);
+    finalmux(low, high, s[1], y);
+endmodule
+
+module mux4_12(input logic[11:0]d0, d1, d2, d3,
+               input logic[1:0]s,
+               output logic[11:0]y);
+    logic[11:0]low, high;
+
+    mux2 #(12) lowmux(d0, d1, s[0], low);
+    mux2 #(12) highmux(d2, d3, s[0], high);
+    mux2 #(12) finalmux(low, high, [s1], y);
+endmodule

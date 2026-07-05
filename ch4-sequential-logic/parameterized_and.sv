37fcfca76645d70294b07feb408bc9b4b91e35b2
Kareem Taiye <olayinkahassan117@gmail.com>
2026-07-05 07:34:57 +0100

    Added parameterized AND reduction module

--- ch4-sequential-logic/parameterized_and.sv
+++ ch4-sequential-logic/parameterized_and.sv
@@ -5,19 +5,17 @@ module andN #(
         output logic y
     );
 
-    //A bus for the internal connecting wires
-    logic chain[N-1:0];
+    logic x[N-1:0];
 
-    // The first input goes directly into the start of the chain
-    assign chain[0] = a[0];
 
     genvar i;
 
     generate
+        assign x[0] = a[0];
         for(i = 1; i < N; i = i + 1) begin: forloop
-          assign chain[i] = chain[i-1] & a[i]
+          assign x[i] = x[i-1] & a[i]
         end
     endgenerate
     
-    assign y = chain[N-1]
+    assign y = x[N-1]
 endmodule

f8852d67b133d49c32c04be0a7bb169162a92d82
Kareem Taiye <olayinkahassan117@gmail.com>
2026-07-23 15:48:50 +0100

    Add assembly immediate operate instruction ex and equiv c code

---
+++ ch6-risc-v-architecture/06_imm-operands/imm_op.c
@@ -0,0 +1,7 @@
+int main() {
+    int a, b;
+    a = a + 4;
+    b =  a -14;
+
+     return 0;
+}
---
+++ ch6-risc-v-architecture/06_imm-operands/imm_op.s
@@ -0,0 +1,3 @@
+# s0 = a, s1 = b
+addi s0, s0, 4      # a = a + 4
+addi s1, s0, -14    # b = a  -14

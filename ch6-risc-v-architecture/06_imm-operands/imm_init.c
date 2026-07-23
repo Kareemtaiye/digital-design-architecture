05e88f1dbd7356cc4aa85c97cb846c8c616cb4a9
Kareem Taiye <olayinkahassan117@gmail.com>
2026-07-23 15:49:25 +0100

    Add assembly immediate initialization instruction ex and equiv c code

---
+++ ch6-risc-v-architecture/06_imm-operands/imm_init.c
@@ -0,0 +1,8 @@
+int main() {
+    int i, x, y;
+    i = 0;
+    x = 2032;
+    y = -78;
+
+    return 0;
+}
---
+++ ch6-risc-v-architecture/06_imm-operands/imm_init.s
@@ -0,0 +1,4 @@
+# s4 = i, s5 = x, s6 = y
+addi s4, zero, 0
+addi s5, zero, 2032
+addi s6, zero, -7

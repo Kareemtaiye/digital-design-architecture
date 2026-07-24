77df67305262c687e27a5bc6ec2951fa23a5b84c
Kareem Taiye <olayinkahassan117@gmail.com>
2026-07-24 20:48:55 +0100

    add the assembly instructions for logical operation(and, or, xor, not) between registers, masking  and setting registers and the C code implementation

---
+++ ch6-risc-v-architecture/09_logical-operations/logical_op.c
@@ -0,0 +1,29 @@
+#include <stdint.h>
+#include <stdio.h>
+
+int main() {
+    uint32_t s0 = 0x12345678;
+    uint32_t s1 = 0x46A11B77;
+    uint32_t s2 = 0xFFFF0000;
+
+    // Bitwise AND, OR, and XOR between registers
+    uint32_t s3 = s1 & s2; // Mask lower 16 bits (clear them)
+    uint32_t s4 = s1 | s2;  // Force upper 16 bits to 1
+    uint32_t s5 = s1 ^ s2; //Toggle upper 16 bits
+
+    //Clear (mask) Bit 3 using AND with 0xFF7 (-9 in decimal)
+    // 0xFF7 in 12-bit is 1111 1111 0111 (Bit 3 is 0)
+    uint32_t s6 = s0 & 0xFFFFFFF7;
+
+    //Set Bit 5 using OR with 0x020 (32 in decimal)
+    // 0x020 has bit 5 set to 1
+    uint32_t s7 = s0 | 0x020;
+
+    // 4. Invert (NOT) all bits of s1 using XOR with -1 (0xFFFFFFFF)
+    uint32_t s8 = ~s1;
+
+
+    printf("s3: %x \n s4: %x \n s5: %x \n s6: %x \n s7: %x \n s8: %x \n", s3, s4, s5, s6, s7, s8);
+
+    return 0;
+}
---
+++ ch6-risc-v-architecture/09_logical-operations/logical_op.s
@@ -0,0 +1,23 @@
+# load test values into registers
+lui s0, 0x12345		# Upper 20 bits
+addi s0, s0, 0x678	# s0 = 0x12345678
+
+lui s1, 0x46A11		# Upper 20 bits
+addi s1, s1, 0xB77	# s1 = 0x46A11B77
+
+lui s2, 0xFFFF0		# s2 = 0XFFFF0000
+
+# Basic register-to-register operations
+and s3, s1, s2		# s3 = s1 & s2 
+or s4, s1, s2		# s4 = s1 | s2
+xor s5, s1, s2		# s3 = s1 ^ s2 
+
+# Masking Bit 3
+and s6, s0, 0xFF7	# s0 = s0 & ~~bit3
+
+# Setting bit 5
+or s7, s0, 0x20		#s7 = s0 | (1 << 5)
+
+# Inverting s1
+xori s8, s1, -1		# s8 = ~s1 (One's complement of s1)
+

a443621d0e665ce1d0e19c4ea4e3c9a16b6875fd
Kareem Taiye <olayinkahassan117@gmail.com>
2026-07-26 00:16:24 +0100

    Add if risc-v assembly code and the C code equivalent

---
+++ ch6-risc-v-architecture/12_conditional/if.c
@@ -0,0 +1,7 @@
+int main() {
+    char apples[10]  = "apples";
+    char oranges[10]  = "oranges";
+
+    return 0;
+}
+
---
+++ ch6-risc-v-architecture/12_conditional/if.s
@@ -0,0 +1,10 @@
+.globl main
+
+#s0 = apples, s1 = oranges s2 = f, s3 = g, s4 = h
+main:
+     bne s0, s1, L1 #skip if (apples != oranges)
+     add s2, s3, s4
+    L1:
+     sub s0, s1, s4 # apples = oranges - h
+
+    

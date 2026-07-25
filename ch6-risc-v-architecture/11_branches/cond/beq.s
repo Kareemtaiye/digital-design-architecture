1abf9327ee324b6167f1afb36b7da7cabd75d575
Kareem Taiye <olayinkahassan117@gmail.com>
2026-07-26 00:03:53 +0100

    Add risc-v assembly program for beq and bne examples

---
+++ ch6-risc-v-architecture/11_branches/cond/beq.s
@@ -0,0 +1,8 @@
+ addi s0, zero, 4 # s0 = 0 + 4 = 4
+ addi s1, zero, 1 # s1 = 0 + 1 = 1
+ slli s1, s1, 2  # s1 = 1 << 2 = 4
+ beq s0, s1, target # s0 = = s1, so branch is taken
+ add s1, s1, 1 # not executed
+ sub s1, s1, s0 # not executed
+target: 
+ add s1, s1, s0 # s1 = 4 + 4 = 8

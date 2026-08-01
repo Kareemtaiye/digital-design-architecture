6e63a8deba8c66651a6cb37af0dd26bc2537563c
Kareem Taiye <olayinkahassan117@gmail.com>
2026-08-01 23:29:30 +0100

    add f function

---
+++ ch6-risc-v-architecture/16_functions/non-leaf/f.c
@@ -0,0 +1,19 @@
+int f1(int a, int b) {
+    int i, x;
+
+
+    x = (a + b) * (a - b);
+
+    for(i = 0; i < a; i++) {
+        x = x + f2(b + i);
+    }
+
+    return x;
+}
+
+int f2(int p) {
+    int r;
+
+    r = p + 5;
+    return r + p;
+}

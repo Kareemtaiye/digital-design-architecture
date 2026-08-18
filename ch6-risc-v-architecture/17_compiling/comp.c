404672367e0ba32528b134bf3900c36b1e451518
Kareem Taiye <olayinkahassan117@gmail.com>
2026-08-18 18:49:24 +0100

    add: risc-v assembly for recursion function

---
+++ ch6-risc-v-architecture/17_compiling/comp.c
@@ -0,0 +1,17 @@
+int func(int a, int b) {
+    
+    if (b < 0) {
+        return a + b;
+    } else {
+        return (a + func(a, b - 1));
+    }
+}
+
+void main() {
+    int f, g, y;
+    
+    f = 2;
+    g = 3;
+    y = func(f, g);
+    return;
+}
